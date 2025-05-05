6760e189eadc9f2e6a8f67f27b6bcb27e0dcb016
Pranav <46899146+Bigorno12@users.noreply.github.com>
2025-05-05 16:26:07 +0400

    remove unused ApplicationModule annotation from package-info.java

--- src/main/java/mu/elca/modulith/order/repo/package-info.java
+++ src/main/java/mu/elca/modulith/order/repo/package-info.java
@@ -1,6 +1,4 @@
 @NamedInterface(name = "repo-order")
-@ApplicationModule(allowedDependencies = {"order::model-order"})
 package mu.elca.modulith.order.repo;
 
-import org.springframework.modulith.ApplicationModule;
 import org.springframework.modulith.NamedInterface;
